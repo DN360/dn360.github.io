cd299a688648acc659605ec6ca119ca873b8060e
Mon Nagata <mon_nagata@hotmail.com>
2021-04-18 00:09:32 +0900

    feat: 履歴ページ実装

--- pages/history.tsx
+++ pages/history.tsx
@@ -4,6 +4,18 @@ import {NextPage} from 'next';
 import Head from 'next/head';
 import {BackButton} from 'components/Buttons';
 
+const tableContents: string[][] = [
+  ['2007-12-16', 'サークル発足'],
+  ['---', '-----'],
+  ['2017-09-11', 'ホームページ新設'],
+  ['2017-11', 'うさぽんダッシュ設置'],
+  ['2018-09', 'ホームページリフォーム終了'],
+  ['2020-02', 'さらにリフォームし、相互リンクページを新設'],
+  ['2020-02-28', 'ハートライトステッキ制御ページを改装'],
+  ['2021-04-14', 'ホームページ改装スタート'],
+  ['2021-04-18', '一旦github pagesに移行し、履歴ページまでを実装'],
+].reverse();
+
 const HistoryPage: NextPage<{}> = () => {
   return (
     <div className={classes.root}>
@@ -11,9 +23,22 @@ const HistoryPage: NextPage<{}> = () => {
         <title>Monbrand software | History</title>
       </Head>
       <BackButton />
+      <div className={classes.center}>
+        <h1 className={classes.h5}>サイト更新履歴</h1>
+      </div>
       <p>
-        まだ書いている途中です。ちょっとまっててね。
+        このサイトの更新履歴です。
       </p>
+      <table className={classes.historyTable}>
+        <tbody>
+          {tableContents.map((toc, i) => (
+            <tr key={'history-' + i}>
+              <th>{toc[0]}</th>
+              <td>{toc[1]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
