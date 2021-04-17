477c11afd32d371a3bfa3d3dcc55bdd2d0747513
Mon Nagata <mon_nagata@hotmail.com>
2021-04-17 23:39:27 +0900

    export時にstyleが崩れるのをパワーでかいけつ

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import classes from '../styles/Background.module.scss';
 import {animated, SpringValue, Transition} from 'react-spring';
 import {NextComponentType, NextPageContext} from 'next';
@@ -20,6 +20,25 @@ const outerProps = {
 const colorLayers = new Array(outerProps.maxLayer * 3).fill(0).map((_, i) => (Math.sin(Math.PI / 180 * (88 + Math.random() * 2) * i / 3) + 1) / 2);
 
 export const Background: React.FC<{items: BackgroundItem[]}> = ({items, children}) => {
+  const prevPageStyleRef = useRef<HTMLStyleElement>(null);
+  const nextPageStyleRef = useRef<HTMLStyleElement>(null);
+  const curtainRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const stylesInHead = Array.from(document.querySelectorAll('style')).filter((el) => el.dataset?.nHref && el.dataset?.nHref !== '');
+    const targetStyle = stylesInHead.filter((el) => el.media !== 'x')[0];
+    if (targetStyle === undefined) {
+      return;
+    }
+    if (nextPageStyleRef.current !== null) {
+      // 前ページが表示されているときは、前のページスタイルを有効にする。
+      targetStyle.media = '';
+      prevPageStyleRef.current = nextPageStyleRef.current;
+      prevPageStyleRef.current.media = '';
+    }
+    nextPageStyleRef.current = targetStyle;
+    setTimeout(() => {
+    }, outerProps.duration);
+  }, [items[0].key]);
   return (
     <div className={classes.root}>
       <Transition from={{
@@ -35,6 +54,28 @@ export const Background: React.FC<{items: BackgroundItem[]}> = ({items, children
       }}
       items={items}
       keys={(item: BackgroundItem) => item.key}
+      onChange={() => {
+        if (curtainRef.current !== null) {
+          const transFunc = () => {
+            // 遷移後のページのスタイルを有効にする
+            if (nextPageStyleRef.current !== null) {
+              nextPageStyleRef.current.media = '';
+            }
+            if (prevPageStyleRef.current !== null) {
+              prevPageStyleRef.current.media = 'x';
+            }
+          };
+          if (curtainRef.current.dataset?.z !== null) {
+            const [x, y] = curtainRef.current.dataset.z.split(',').map(Number);
+            if (y > 0 && x <= 0.5) {
+              transFunc();
+            }
+            if (y <= 0 && x >= 0.5) {
+              transFunc();
+            }
+          }
+        }
+      }}
       >
         {
           (animatedProps: {[name: string]: SpringValue}, {pageProps, Component}) => {
@@ -81,7 +122,7 @@ export const Background: React.FC<{items: BackgroundItem[]}> = ({items, children
             const sizeFunc = (layer: number) => `${MAX_WIDTH * layer / MAX_LAYER * 2}px`;
             outerProps.initialized = true;
             const recursiveFunc = (layer: number) => layer >= MAX_LAYER ? (<></>) : (
-              <animated.div style={{
+              <animated.div ref={layer === 0 ? curtainRef : null} style={{
                 ...curtainStyle,
                 transform: layer === 0 ? animatedProps.z.to((x: number, y: number) => `rotateZ(${y > 0 ? '-' : ''}${x * 90 * 1 + 45}deg)`) as any : 'none',
                 display: animatedProps.z.to((x: number) => x >= 1 ? 'none' : 'block') as any,
@@ -90,7 +131,7 @@ export const Background: React.FC<{items: BackgroundItem[]}> = ({items, children
                 margin: animatedProps.z.to((x: number) => marginFunc(forceVisibled ? 1 - func(x, layer) : 1 - func(x, layer), layer)) as any,
                 width: animatedProps.z.to(() => sizeFunc(layer)) as any,
                 height: animatedProps.z.to(() => sizeFunc(layer)) as any,
-              }}>
+              }} data-z={layer === 0 ? animatedProps.z : 'null'}>
                 {recursiveFunc(layer + 1)}
               </animated.div>
             );
