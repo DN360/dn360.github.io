5a5954005e306567699ae204799f1c7e6a9fdfc0
Mon Nagata <mon_nagata@hotmail.com>
2021-04-18 00:03:27 +0900

    feat: linkページ

--- pages/links.tsx
+++ pages/links.tsx
@@ -3,6 +3,28 @@ import classes from 'styles//links.module.scss';
 import {NextPage} from 'next';
 import Head from 'next/head';
 import {BackButton} from 'components/Buttons';
+import {TabLink} from 'components/TabLink';
+
+const links: [
+  string, string, string | Element | React.ReactFragment
+][] = [
+  ['アイマスPカード', 'https://wiki.mbsoftware.tokyo/card.html', 'おひげPのアイマスPカードです、よろしくおねがいします。'],
+  ['あずきノート', 'https://uxhpu.net/', (
+    <span key="link-to-azuki">
+      <TabLink className={classes.link} href="https://twitter.com/uxhpu">あずきちゃん</TabLink>のホームページです。
+    </span>
+  )],
+  ['靴紐とトイレと竜田丼', 'http://shoelaceart.starfree.jp/uec/', (
+    <span key="link-to-atari">
+      <TabLink className={classes.link} href="https://twitter.com/ebioishii_u">淵野アタリ</TabLink>くんのホームページです。が、どうやらリンク切れしています。
+    </span>
+  )],
+  ['つまみネット', 'https://www.trpfrog.net/', (
+    <span key="link-to-tsumami">
+      <TabLink className={classes.link} href="https://twitter.com/TrpFrog">つまみ</TabLink>くんのホームページです。
+    </span>
+  )],
+];
 
 const LinksPage: NextPage<{}> = () => {
   return (
@@ -11,9 +33,22 @@ const LinksPage: NextPage<{}> = () => {
         <title>Monbrand software | Links</title>
       </Head>
       <BackButton />
+      <div className={classes.center}>
+        <h1 className={classes.h5}>リンク集</h1>
+      </div>
       <p>
-        まだ書いている途中です。ちょっとまっててね。
+        相互リンクをしている or していたリンク集です。相互リンクはお会いしたことがある方ならお気軽にご連絡をどうぞ。
       </p>
+      <table className={classes.linkTable}>
+        <tbody>
+          {links.map((link) => (
+            <tr key={link[1]}>
+              <th><TabLink href={link[1]}>{link[0]}</TabLink></th>
+              <td>{link[2]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
